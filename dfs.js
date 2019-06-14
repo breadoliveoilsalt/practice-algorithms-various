3de0c68e151629eebc64a18f12b4e19efa860573
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-06-14 14:46:26 -0400

    Add additional dfs in js. To be tested.

--- dfs.js
+++ dfs.js
@@ -9,4 +9,32 @@ function dfs(root, target) {
   let right = root.right ? dfs(root.right) : null
 
   return left || right
+
+}
+
+function dfs(graph, target) {
+
+  let nodesSeen = new Set()
+  nodesSeen.add(graph[0])
+
+  let stack = []
+  stack.push(graph[0])
+
+  while (stack.length > 0 ) {
+    let currentNode = stack.pop()
+    if (currentNode.value === target) {
+      return currentNode
+    }
+    currentNode.neighbors.forEach( neighbor => {
+      if (nodesSeen.has(neighbor)) {
+        next
+      } else {
+        nodesSeen.add(neighbor)
+        stack.push(neighbor)
+      }
+    })
+  }
+
+  return false
+
 }
